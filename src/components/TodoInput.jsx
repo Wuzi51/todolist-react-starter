e31ca15eeab0ee3ed2727210718d263e643d4857
Richy <wuzi50115@gmail.com>
2024-08-25 15:44:09 +0800

    feat: add enter key press event handler style: add active class when input value length > 0

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import styled from 'styled-components';
 
 const StyledAddTodoContainer = styled.div`
@@ -69,20 +70,32 @@ const StyledAddTodoActionContainer = styled.div`
 `;
 const TodoInput = ({inputValue, onChange, onKeyDone, onAddTodo}) => {
   return (
-    <StyledAddTodoContainer>
+    <StyledAddTodoContainer
+      className={clsx('', { active: inputValue.length > 0 })}
+    >
       <StyledLabelIcon className="icon" htmlFor="add-todo-input" />
       <StyledInputContainer>
-        <input id="add-todo-input" 
-        type="text" 
-        placeholder="新增工作" 
-        value={inputValue}
-        onChange={e => {
-          onChange?.(e.target.value)
-        }}
+        <input
+          id="add-todo-input"
+          type="text"
+          placeholder="新增工作"
+          value={inputValue}
+          onChange={(e) => {
+            onChange?.(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              onKeyDone?.();
+            }
+          }}
         />
       </StyledInputContainer>
-      <StyledAddTodoActionContainer>
-        <button className="btn-reset" onClick={() => onAddTodo?.()}>新增</button>
+      <StyledAddTodoActionContainer
+        className={clsx('', { active: inputValue.length > 0 })}
+      >
+        <button className="btn-reset" onClick={() => onAddTodo?.()}>
+          新增
+        </button>
       </StyledAddTodoActionContainer>
     </StyledAddTodoContainer>
   );
